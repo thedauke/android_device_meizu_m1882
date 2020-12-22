a99d436d3ae75e030fda99aba331164d66016e02
TRANSAERO21 <martynenkodima21@gmail.com>
2020-12-23 01:26:55 +0600

    m1882: Build sensors HAL :factory:

--- device.mk
+++ device.mk
@@ -238,8 +238,9 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
- #    android.hardware.sensors@1.0-service.m1882 \
-    libsensorndkbridge
+    libsensorndkbridge \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Telephony
 PRODUCT_PACKAGES += \
