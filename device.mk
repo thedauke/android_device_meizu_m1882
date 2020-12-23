a13698f6f71034f34b6fcb61bf8731b7cd475c87
TRANSAERO21 <martynenkodima21@gmail.com>
2020-12-23 16:08:35 +0600

    m1882: Import powerhint from stock :zap:

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
